ff69c32127ad9cb475a37edcabc9919123ebc580
purgeyao <zhaoy@deepblueai.com>
2019-06-25 18:12:34 +0800

    upd rabbitmq-cluster.sh version

--- service/docker-start/cluster.sh
+++ service/docker-start/cluster.sh
@@ -2,20 +2,20 @@
 # 将RabbitMQ节点加入到集群中
 
 # 1
-docker exec -it myrabbit1 bash
+docker exec -it rabbitcluster1 bash
 rabbitmqctl stop_app
 rabbitmqctl reset
 rabbitmqctl start_app
 exit
 # 2
-docker exec -it myrabbit2 bash
+docker exec -it rabbitcluster2 bash
 rabbitmqctl stop_app
 rabbitmqctl reset
 rabbitmqctl join_cluster --ram rabbit@rabbit1
 rabbitmqctl start_app
 exit
 # 3
-docker exec -it myrabbit3 bash
+docker exec -it rabbitcluster3 bash
 rabbitmqctl stop_app
 rabbitmqctl reset
 rabbitmqctl join_cluster --ram rabbit@rabbit1
--- service/docker-start/rabbitmq-cluster.sh
+++ service/docker-start/rabbitmq-cluster.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # 创建多个RabbitMQ容器
-docker run -d --hostname rabbit1 --name rabbit1 -p 15672:15672 -p 5672:5672 -e RABBITMQ_ERLANG_COOKIE='rabbitcookie' rabbitmq:3.7.8-management
+docker run -d --hostname rabbit1 --name rabbitcluster1 -p 15672:15672 -p 5672:5672 -e RABBITMQ_ERLANG_COOKIE='rabbitcookie' rabbitmq:3.7.15-management
 
-docker run -d --hostname rabbit2 --name rabbit2 -p 5673:5672 -p 15673:15672 --link myrabbit1:rabbit1 -e RABBITMQ_ERLANG_COOKIE='rabbitcookie' rabbitmq:3.7.8-management
+docker run -d --hostname rabbit2 --name rabbitcluster2 -p 5673:5672 -p 15673:15672 --link rabbitcluster1:rabbit1 -e RABBITMQ_ERLANG_COOKIE='rabbitcookie' rabbitmq:3.7.15-management
 
-docker run -d --hostname rabbit3 --name rabbit3 -p 5674:5672 -p 15674:15672 --link myrabbit1:rabbit1 --link myrabbit2:rabbit2 -e RABBITMQ_ERLANG_COOKIE='rabbitcookie' rabbitmq:3.7.8-management
+docker run -d --hostname rabbit3 --name rabbitcluster3 -p 5674:5672 -p 15674:15672 --link rabbitcluster1:rabbit1 --link rabbitcluster2:rabbit2 -e RABBITMQ_ERLANG_COOKIE='rabbitcookie' rabbitmq:3.7.15-management
 
